ff333726891f591f6848e3e9c7b60dfdeab9b30c
Yair <32864499+bluepostit@users.noreply.github.com>
2020-08-15 00:29:47 +0300

    setup build with webpack & babel

---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const path = require('path')
+
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin')
+const nodeExternals = require('webpack-node-externals')
+
+module.exports = {
+  entry: './src/index.ts',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+
+  devtool: 'source-map',
+
+  resolve: {
+    extensions: ['.webpack.js', '.web.ts', '.ts', '.tsx', '.js', '.jsx']
+  },
+
+  target: 'node',
+
+  externals: [nodeExternals()],
+
+  module: {
+    rules: [
+      // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
+      {
+        test: /\.tsx?$/,
+        loader: "awesome-typescript-loader"
+      },
+
+      // {
+      //   test: /\.(ts|js)x?$/,
+      //   exclude: /node_modules/,
+      //   use: {
+      //     loader: 'babel-loader',
+      //     options: {
+      //       cacheDirectory: true
+      //     }
+      //   }
+      // },
+
+      // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "source-map-loader"
+      }
+    ]
+  },
+
+  plugins: [
+    new ForkTsCheckerWebpackPlugin()
+  ]
+}
