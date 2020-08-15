7cf5529b61bed1a83f5419d5e6bf504d4ff3c861
Yair <32864499+bluepostit@users.noreply.github.com>
2020-08-15 17:10:52 +0300

    add relation mapping fields for item

--- src/models/item.ts
+++ src/models/item.ts
@@ -5,5 +5,40 @@ export default class Item extends Model {
   name!: string
   description!: string
 
+  parent?: Item
+  children?: Item[]
+
   static tableName = 'items'
+
+  static jsonSchema = {
+    type: 'object',
+    required: ['name', 'description'],
+
+    properties: {
+      id: { type: 'integer' },
+      parent_id: { type: ['integer', 'null'] },
+      name: { type: 'string', minLength: 1, maxLength: 255 },
+      description: { type: 'string', minLength: 1, maxLength: 255 }
+    }
+  }
+
+  static relationMappings = () => ({
+    parent: {
+      relation: Model.BelongsToOneRelation,
+      modelClass: Item,
+      join: {
+        from: 'items.parent_id',
+        to: 'items.id'
+      }
+    },
+
+    children: {
+      relation: Model.HasManyRelation,
+      modelClass: Item,
+      join: {
+        from: 'persons.id',
+        to: 'persons.parent_id'
+      }
+    }
+  })
 }
