0b5e18576e787e1bc5e804cb018ee5b0f61aac08
Yair <32864499+bluepostit@users.noreply.github.com>
2020-08-23 11:04:11 +0300

    add hook: authorize entity

--- src/plugins/authentication.ts
+++ src/plugins/authentication.ts
@@ -1,5 +1,6 @@
 import { FastifyPlugin, preHandlerHookHandler } from 'fastify'
 import fp from 'fastify-plugin'
+import { User, BelongsToUser } from '../models'
 
 const plugin: FastifyPlugin = (fastify, _options, done) => {
   const mustBeSignedIn: preHandlerHookHandler = (request, _reply, done) => {
@@ -10,8 +11,25 @@ const plugin: FastifyPlugin = (fastify, _options, done) => {
     done()
   }
 
+  const authorizeEntity: preHandlerHookHandler = (request, _reply, done) => {
+    const entity = (request.entity as unknown) as BelongsToUser
+    if (!entity) {
+      done(fastify.httpErrors.badRequest('No entity given'))
+    }
+    if (request.session.user) {
+      const user = (request.session.user as User)
+      if (user.id !== entity.user_id) {
+        fastify.log.info(`user ID: ${user.id} != entity's user ID: ${entity.user_id}`)
+        done(fastify.httpErrors.forbidden(
+          'This entity does not belong to you'
+        ))
+      }
+    }
+  }
+
   fastify.decorate('auth', {
     mustBeSignedIn,
+    authorizeEntity
   })
 
   done()
@@ -20,6 +38,7 @@ const plugin: FastifyPlugin = (fastify, _options, done) => {
 declare module 'fastify' {
   interface FastifyInstance {
     auth: {
+      authorizeEntity: preHandlerHookHandler,
       mustBeSignedIn: preHandlerHookHandler
     }
   }
