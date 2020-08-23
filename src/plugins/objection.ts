262a85031df0b6ec13d102eb518f8db9f8dd8e4d
Yair <32864499+bluepostit@users.noreply.github.com>
2020-08-23 11:02:51 +0300

    hook to set entity on request

--- src/plugins/objection.ts
+++ src/plugins/objection.ts
@@ -1,19 +1,52 @@
-import { FastifyPlugin } from 'fastify'
+import { FastifyPlugin, preHandlerHookHandler } from 'fastify'
 import fp from 'fastify-plugin'
 import Knex from 'knex'
 import { Model } from 'objection'
 import knex from '../config/knex'
 import * as models from '../models'
 
+interface RequestWithId {
+  id: number
+}
+
 const plugin: FastifyPlugin = async (fastify, _options, _done) => {
-  fastify.decorate("db", {
+  const setEntity = (entityClass: typeof Model) => {
+    const fn: preHandlerHookHandler = async (request, _reply, done) => {
+      const params = request.params as RequestWithId
+      if (!params || !params.id) {
+        const error = fastify.httpErrors.badRequest(
+          `You must provide an ID for this ${entityClass.name}`
+        )
+        done(error)
+      }
+      let query = entityClass.query().findById(params.id)
+      if (entityClass.modifiers['defaultSelects']) {
+        query = query.modify('defaultSelects')
+      }
+      const entity = await query
+      if (!entity) {
+        const error = fastify.httpErrors.notFound(
+          `No ${entityClass.name} found with that ID`
+        )
+        done(error)
+      }
+      request.entity = entity
+      done()
+    }
+    return fn
+  }
+
+  fastify.decorateRequest('entity', '')
+
+  fastify.decorate('db', {
     knex,
-    models
+    models,
+    setEntity,
   })
 }
 
 export default fp(plugin, {
-  name: 'db'
+  name: 'db',
 })
 
 declare module 'fastify' {
@@ -23,6 +56,24 @@ declare module 'fastify' {
       models: {
         [index: string]: typeof Model
       }
+      /**
+       * Get a hook function which looks up an entity in the database by its
+       * id (as specified in `request.params`).
+       * The found entity will be set as `request.entity`.
+       * If no entity is found, or if no id is given in `request.params`,
+       * an error will be thrown.
+       *
+       * @param entityClass the class of the entity to use when retrieving
+       * @returns preHandler hook function, curried to use `entityClass`
+       */
+      setEntity(entityClass: typeof Model): preHandlerHookHandler
     }
+  }
+
+  interface FastifyRequest {
+    /**
+     * A `Model` instance representing the entity requested by its id.
+     */
+    entity: Model | undefined
   }
 }
